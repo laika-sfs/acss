d7c7895f1ba8b9ccca684cbcc71bd3254fc640d5
laika-sfs <108960783+laika-sfs@users.noreply.github.com>
2022-07-09 23:05:26 -0400

    Removed commented code

--- Apps/ACSS.Api/Data/PlanetContext.cs
+++ Apps/ACSS.Api/Data/PlanetContext.cs
@@ -50,15 +50,6 @@ namespace ACSS.Api.Data
         public virtual DbSet<TextureFormula> TextureFormula { get; set; } = null!;
         public virtual DbSet<TextureSize> TextureSize { get; set; } = null!;
 
-        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //        {
-        //            if (!optionsBuilder.IsConfigured)
-        //            {
-        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        //                optionsBuilder.UseSqlServer("Data Source=192.168.2.90\\DEV;Initial Catalog=SFS;User ID=sa;Password=hosannah7PADDY4sash_pedestal");
-        //            }
-        //        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<FogKey>(entity =>
